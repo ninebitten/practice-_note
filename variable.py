4bcaf421b11e9a56355271e38200eca3178195ca
ninebitten <114128298+ninebitten@users.noreply.github.com>
2023-04-18 16:46:04 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,28 @@
+x ="python is awsome"
+print(x)
+
+
+val1 = 3
+val2 = 2
+
+# using the modulus operator
+res = val1 % val2
+print(res)
+
+
+
+val1 = 3
+val2 = 2
+
+# using the division operator
+res = val1 / val2
+print(res)
+
+
+
+val1 = 2
+val2 = 3
+
+# using the multiplication operator
+res = val1 * val2
+print(res)
