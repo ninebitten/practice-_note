48632445eb745d10cb3081534668f49d16783fb6
Sabbir <stat1996.bd.iu@gmail.com>
2023-04-19 15:10:45 +0600

    second

---
+++ list.py
@@ -0,0 +1,20 @@
+mylist = ["banana", "cherry", "apple"]
+print(mylist)
+
+item = mylist[0]
+print(item)
+if "banana" in mylist:
+    print("yes")
+else:
+    print("no")
+    
+    
+    if "lemon" in mylist:
+        print("yes")
+    else:
+        print("no")
+        
+        #adding a new string
+        
+        mylist.append("lemon")
+        print(mylist)
---
+++ oper.py
@@ -0,0 +1,13 @@
+a=34
+b=56
+print(a+b)
+print(a-b)
+print(10%3)
+
+
+val1 = 3
+val2 = 2
+  
+# using the floor division
+res = val1 // val2
+print(res)
