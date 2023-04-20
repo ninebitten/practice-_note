c8502bc6f4e30c758eaf2e3384debe9ba8296903
Sabbir <stat1996.bd.iu@gmail.com>
2023-04-21 00:50:18 +0600

    operator

--- list.py
+++ list.py
@@ -1,20 +1,36 @@
-mylist = ["banana", "cherry", "apple"]
+mylist = ["fk", "ffg", "ffgg"]
 print(mylist)
 
-item = mylist[0]
-print(item)
-if "banana" in mylist:
-    print("yes")
-else:
-    print("no")
-    
-    
-    if "lemon" in mylist:
-        print("yes")
-    else:
-        print("no")
-        
-        #adding a new string
-        
-        mylist.append("lemon")
-        print(mylist)
+
+
+
+
+# Addition
+a = 5
+b = 7
+c = a + b
+print(c)  # Output: 12
+
+# Subtraction
+x = 10
+y = 3
+z = x - y
+print(z)  # Output: 7
+
+# Multiplication
+p = 3
+q = 6
+r = p * q
+print(r)  # Output: 18
+
+# Division
+m = 20
+n = 4
+o = m / n
+print(o)  # Output: 5.0 (Note: division always returns a float in Python)
+
+# Modulus
+e = 16
+f = 3
+g = e % f
+print(g)  # Output: 1 (modulus returns the remainder of the division operation)
--- variable.py
+++ variable.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 x ="python is awsome"
 print(x)
 
@@ -27,33 +26,3 @@ val2 = 3
 # using the multiplication operator
 res = val1 * val2
 print(res)
-=======
-x ="python is awsome"
-print(x)
-
-
-val1 = 3
-val2 = 2
-
-# using the modulus operator
-res = val1 % val2
-print(res)
-
-
-
-val1 = 3
-val2 = 2
-
-# using the division operator
-res = val1 / val2
-print(res)
-
-
-
-val1 = 2
-val2 = 3
-
-# using the multiplication operator
-res = val1 * val2
-print(res)
->>>>>>> 4bcaf421b11e9a56355271e38200eca3178195ca
